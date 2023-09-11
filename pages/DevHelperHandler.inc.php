6ffca0a9973f42269bd8ffe3f64d1cb88b5ae7ec
Lutz Prechelt <prechelt@inf.fu-berlin.de>
2023-09-11 12:59:25 +0200

    DevHelperHandler.inc.php: many restructurings

--- pages/DevHelperHandler.inc.php
+++ pages/DevHelperHandler.inc.php
@@ -3,14 +3,13 @@
 /**
  * @file plugins/generic/reviewqualitycollector/pages/DevHelperHandler.inc.php
  *
- * Copyright (c) 2014-2017 Simon Fraser University
- * Copyright (c) 2018-2019 Lutz Prechelt
+ * Copyright (c) 2018-2023 Lutz Prechelt
  * Distributed under the GNU General Public License, Version 3.
  *
  * @class DevHelperHandler
  * @ingroup plugins_generic_reviewqualitycollector
  *
- * @brief Handle requests to show what OJS-to-RQC requests will look like.
+ * @brief Handle requests to show what OJS-to-RQC requests will look like or make one "by hand".
  */
 
 /* for OJS 3.4:
@@ -20,72 +19,48 @@ use PKP\db\DAORegistry;
 use PKP\plugins\PluginRegistry;
 */
 
-class DevHelperHandler extends Handler {
+class DevHelperHandler extends Handler
+{
 	var $plugin;
 
-	function __construct() {
+	function __construct()
+	{
 		parent::__construct();
 		$this->plugin = PluginRegistry::getPlugin('generic', 'rqcplugin');
 	}
 
 	/**
 	 * Show RQC request corresponding to a given submissionId (args[0]) (with ?viewonly=1) or
-	 * make the RQC request and show errors or perform the RQC redirect (with ?viewonly=0).
+	 * make the RQC request and show errors or perform the RQC redirect (with ?viewonly=0&stageId=3).
 	 */
-	function rqccall($args, $request) {
-		$show_confirmation_page = true;  // hardcoded behavior switch
+	function rqccall($args, $request)
+	{
 		//----- prepare processing:
 		$router = $request->getRouter();
-		$requestArgs = getQueryArray($request);
-		$context = $request->getContext();
-		$user = $request->getUser();
+		$requestArgs = $this->plugin->getQueryArray($request);
 		$journal = $router->getContext($request);
 		$submissionId = $args[0];
 		$viewOnly = array_key_exists('viewonly', $requestArgs) ? $requestArgs['viewonly'] : true;
-		$rqcJournalId = $this->plugin->getSetting($context->getId(), 'rqcJournalId');
-		$rqcJournalAPIKey = $this->plugin->getSetting($context->getId(), 'rqcJournalAPIKey');
 		if ($viewOnly) {
 			//----- get RQC data:
 			$rqcDataObj = new RqcData();
-			$data = $rqcDataObj->rqcdata_array($request, $journal, $submissionId);
-			//----- add interesting bits:
-			$data['=========='] = "####################";
-			//$data['journal'] = $journal;
-			$data['rqc_journal_id'] = $rqcJournalId;
-			$data['rqc_journal_api_key'] = $rqcJournalAPIKey;
+			$data = $rqcDataObj->rqcdata_array($request, $journal->getId(), $submissionId);
 			//----- produce output:
 			header("Content-Type: application/json; charset=utf-8");
 			//header("Content-Type: text/plain; charset=utf-8");
 			print(json_encode($data, JSON_PRETTY_PRINT));
-		}
-		else {  //----- make an actual RQC call:
-			$result = call_mhs_submission($this->plugin->rqc_server(), $rqcJournalId, $rqcJournalAPIKey,
-										  $request, $journal, $submissionId);
-			$status = $result['status'];
-			$response = $result['response'];
-			if ($status == 303) {  // that's what we expect: redirect
-				if ($show_confirmation_page) {
-					header("Content-Type: text/html; charset=utf-8");
-					$target_url = $response['redirect_target'];
-					$submission_title = $result['request']['title'];
-					print(rqcgrading_confirmation_page($target_url, $submission_title));
-				}
-				else {
-					header("HTTP/1.1 303 See Other");
-					header("Location: " . $response['redirect_target']);
-				}
-			}
-			else {  // hmm, something is wrong: Show the JSON response
-				header("Content-Type: application/json; charset=utf-8");
-				print(json_encode($response, JSON_PRETTY_PRINT));
-			}
+		} else {  //----- make an actual RQC call:
+			$handler = new RqccallHandler();
+			$rqc_result = $handler->sendToRqc($request, $journal->getId(), $submissionId);
+			$handler->processRqcResponse($rqc_result['status'], $rqc_result['response']);
 		}
 	}
 
 	/**
 	 * Make a previously submitted OJS reviewing case RQC-submittable again.
 	 */
-	public function ra_reset($args, $request) {
+	public function ra_reset($args, $request)
+	{
 		header("Content-Type: text/plain; charset=utf-8");
 		$submissionId =& $args[0];
 		$userId = $request->getUser()->getId();
@@ -99,41 +74,17 @@ class DevHelperHandler extends Handler {
 	}
 
 	/**
-	 * Simple "Hello, world!" request. TODO 2: remove?
-	 */
-	public function hello($args, $request) {
-		header("Content-Type: text/plain; charset=utf-8");
-		$rq = array(
-			// "context" => $request->getContext(),
-			"contextId" => $request->getContext()->getId(),
-			"user" => $request->getUser(),
-			"userVars" => $request->getUserVars(),
-			       );
-		return "Hello, this is the request\n" . json_encode($rq, JSON_PRETTY_PRINT)
-			. "\nand the args:\n" . json_encode($args, JSON_PRETTY_PRINT) . "\n";
-	}
-
-	/**
-	 * Temporary helper function for exploring the DAOs.
-	 */
-	protected function getters_of($object) {
-		$getters = array_filter(get_class_methods($object),
-			function($s) { return substr($s, 0, 3) == "get"; });
-		$getters = array_values($getters);  // get rid of keys
-		sort($getters);
-		return $getters;
-	}
-
-	/**
 	  * Make review case (MRC) in the current journal.
 	  * INCOMPLETE AND OUTDATED. TODO 1: remove or rewrite.
 	  */
-	function mrc($args, $request) {
+	/*
+	function mrc($args, $request)
+	{
 		header("Content-Type: text/html; charset=utf-8");
 		echo "START\n";
 		//----- prepare processing:
 		$router = $request->getRouter();
-		$requestArgs = $request->getQueryArray();
+		$requestArgs = $this->plugin->getQueryArray($request);
 		$contextId = $request->getContext()->getId();
 		$user = $request->getUser();
 		$now = time();
@@ -142,7 +93,6 @@ class DevHelperHandler extends Handler {
 		$article->setJournalId($contextId);
 		$article->setTitle("Test submission " . date('Y-m-d H:i:s'), RQC_LOCALE);
 		//$article->sub
-		printf("%s\n", $article->getTitle(RQC_LOCALE));
 		$this->articleDao->insertObject($article);
 		//----- make authors:
 		$author = new Author();
@@ -151,7 +101,6 @@ class DevHelperHandler extends Handler {
 		$author->setEmail("author1@prechelt.dialup.fu-berlin.de");
 		$author->setSubmissionId($article->getId());
 		$this->authorDao->insertObject($author);
-		printf("context: %d\n", $contextId);
 		//----- make review round:
 		//-----	make editor assignments:
 		//----- make reviewer assignments:
@@ -168,18 +117,5 @@ class DevHelperHandler extends Handler {
 		//print(json_encode($data, JSON_PRETTY_PRINT));
 		echo "END.\n";
 	}
-}
-
-/**
- * Workaround for $request->getQueryArray().
- * In OJS 3.3.0, that call produces the error
- * "Application::getContextList() cannot be called statically"
- */
- function getQueryArray($request) {
-	$queryString = $request->getQueryString();
-	$requestArgs = array();
-	if (isset($queryString)) {
-		parse_str($queryString, $requestArgs);
-	}
-	return $requestArgs;
+	*/
 }
