a0fcea16902e21d95baf73bae3faa7a60121f9e6
Lutz Prechelt <prechelt@inf.fu-berlin.de>
2023-09-11 12:21:04 +0200

    RqcData.inc.php: add yet-empty attachment_set; cleanups

--- classes/RqcData.inc.php
+++ classes/RqcData.inc.php
@@ -22,6 +22,7 @@ use PKP\security\Role;
 use PKP\site\VersionCheck;
 */
 import('classes.workflow.EditorDecisionActionsManager');  // decision action constants
+import('lib.pkp.classes.core.PKPPageRouter');
 import('lib.pkp.classes.site.VersionCheck');
 import('plugins.generic.reviewqualitycollector.classes.RqcDevHelper');
 
@@ -30,27 +31,32 @@ import('plugins.generic.reviewqualitycollector.classes.RqcDevHelper');
  * Builds the data object to be sent to the RQC server from the various pieces of the OJS data model:
  * submission, authors, editors, reviewers and reviews, active user, decision, etc.
  */
-class RqcData extends RqcDevHelper {
+class RqcData extends RqcDevHelper
+{
 
 	const CONFIDENTIAL_FIELD_REGEXP = '/[Cc]onfidential/';  // review form fields with such names are excluded
 
 	function __construct() {
-		$this->plugin = PluginRegistry::getPlugin('generic', 'rqcplugin');
+		// $this->plugin = PluginRegistry::getPlugin('generic', 'rqcplugin');
 		parent::__construct();
 	}
 
 	/**
 	 * Build PHP array with the data for an RQC call to be made.
+	 * if $request is null, interactive_user and mhs_submissionpage are transmitted as "".
 	 */
-	function rqcdata_array($request, $journal, $submissionId) {
+	function rqcdata_array($request, $contextId, $submissionId): array
+	{
+		$contextDao = Application::getContextDAO();
 		$reviewRoundDao = DAORegistry::getDAO('ReviewRoundDAO');
 		$submissionDao = DAORegistry::getDAO('SubmissionDAO');
 		//----- prepare processing:
+		$journal = $contextDao->getById($contextId);
 		$submission = $submissionDao->getById($submissionId);
 		$data = array();
 		//----- fundamentals:
-		$data['interactive_user'] = $this->get_interactive_user($request);
-		$data['mhs_submissionpage'] = $request->getRequestUrl();
+		$data['interactive_user'] = $request ? $this->get_interactive_user($request) : "";
+		$data['mhs_submissionpage'] = $request ? $this->get_mhs_submissionpage($request, $submissionId) : "";
 
 
 		//----- submission data:
@@ -67,15 +73,36 @@ class RqcData extends RqcDevHelper {
         $data['author_set'] = $this->get_author_set($submission->getAuthors());
         $data['edassgmt_set'] = $this->get_editorassignment_set($submissionId);
         $data['review_set'] = $this->get_review_set($submissionId, $lastReviewRound);
-        $data['decision'] = $this->get_decision($submission, $lastReviewRound);
+        $data['decision'] = $this->get_decision($lastReviewRound);
 
 		return $data;
 	}
 
 	/**
+	 * Return linear array of RQC-ish attachment objects.
+	 */
+	protected static function get_attachment_set($reviewerSubmission): array
+	{
+		$reviewFilesDao = DAORegistry::getDAO("ReviewFilesDAO");
+		$submissionFileDao = DAORegistry::getDAO("SubmissionFileDAO");
+		$result = array();  // TODO 1: get attachments
+		/* getting attachments is not for the faint-of-heart:
+		   $reviewerSubmission.reviewer_file_id points to
+		   table review_files with attr submission_file_id which points to
+		   table submission_file with attr file_id which points to
+		   table file with attr path in dir files_dir from config.inc.php.
+		   In case you want a filename (the path in file is a hash value),
+		   just look in submission_file_settings, which are I18Ned.
+		   However, ReviewFilesDAO has no retrieval op?
+		*/
+		return $result;
+	}
+
+	/**
 	 * Return linear array of RQC-ish author objects.
 	 */
-	protected static function get_author_set($authorsobjects) {
+	protected static function get_author_set($authorsobjects): array
+	{
 		$result = array();
 		foreach ($authorsobjects as $authorobject) {
 			if (!(bool)$authorobject->getIncludeInBrowse())  // TODO 2: correct like this?
@@ -98,7 +125,8 @@ class RqcData extends RqcDevHelper {
      * We use decisions only and return an 'unknown' if there are only recommendations.
      * We simply use the first decision we see.
      */
-    protected function get_decision($submission, $reviewRound) {
+    protected function get_decision($reviewRound): string
+	{
         // See EditDecisionDAO->getEditorDecisions, $this->rqc_decision
 		$editDecisionDao = DAORegistry::getDAO('EditDecisionDAO');
 		$rr = $reviewRound;
@@ -116,7 +144,8 @@ class RqcData extends RqcDevHelper {
 	/**
 	 * Return linear array of RQC editorship descriptor objects.
 	 */
-	protected function get_editorassignment_set($submissionId) {
+	protected function get_editorassignment_set($submissionId): array
+	{
 		$stageAssignmentDao = DAORegistry::getDAO('StageAssignmentDAO');
 		$userDao = DAORegistry::getDAO('UserDAO');
 		$userGroupDao = DAORegistry::getDAO('UserGroupDAO');
@@ -154,11 +183,20 @@ class RqcData extends RqcDevHelper {
 	 * Return emailaddress of current user or "" if this is not an interactive call.
 	 * The adapter needs to hope this same address is registered with RQC as well.
 	 */
-	protected static function get_interactive_user($request) {
+	protected static function get_interactive_user($request)
+	{
 		$user = $request->getUser();
 		return $user ? $user->getEmail() : "";
 	}
 
+	/**
+	 * Return the URL to which RQC should redirect after grading.
+	 */
+	protected function get_mhs_submissionpage(PKPRequest $request, int $submissionId)
+	{
+		return $request->url(null, 'workflow', 'index',
+			array($submissionId, WORKFLOW_STAGE_ID_EXTERNAL_REVIEW));
+	}
 
 	/**
 	 * Return linear array of RQC review descriptor objects.
@@ -168,7 +206,8 @@ class RqcData extends RqcDevHelper {
 	 * case 2) default review data structure (using SubmissionComment).
 	 * See PKPReviewerReviewStep3Form::saveReviewForm() for details.
 	 */
-	protected function get_review_set($submissionId, $reviewRound) {
+	protected function get_review_set($submissionId, $reviewRound): array
+	{
 		$reviewAssignmentDao = DAORegistry::getDAO('ReviewAssignmentDAO');
 		$reviewerSubmissionDao = DAORegistry::getDAO('ReviewerSubmissionDAO');
 		$userDao = DAORegistry::getDAO('UserDAO');
@@ -179,7 +218,7 @@ class RqcData extends RqcDevHelper {
 			if ($reviewAssignment->getRound() != $reviewRoundN ||
 				$reviewAssignment->getStageId() != WORKFLOW_STAGE_ID_EXTERNAL_REVIEW)
 				continue;  // irrelevant record, skip it.
-			$rqcreview = array();
+			$rqcreview = array();  // will become one entry in the result set
 			$reviewerSubmission = $reviewerSubmissionDao->getReviewerSubmission($reviewId);
 			//--- review metadata:
 			$rqcreview['visible_id'] = $reviewId;
@@ -191,13 +230,14 @@ class RqcData extends RqcDevHelper {
 			$reviewFormId = $reviewAssignment->getReviewFormId();
 			if ($reviewFormId) {  // case 1
 				$reviewtext = $this->getReviewTextFromForm($reviewerSubmission, $reviewFormId);
-				$rqcreview['is_html'] = false;  // TODO 2: is there really no way to get HTML here?
-			}
-			else {  // case 2
+				$is_html = false;  // TODO 2: is there really no way to get HTML here?
+			} else {  // case 2
 				$reviewtext = $this->getReviewTextDefault($reviewAssignment);
-				$rqcreview['is_html'] = true;
+				$is_html = true;
 			}
 			$rqcreview['text'] = $reviewtext;
+			$rqcreview['is_html'] = $is_html;
+			$rqcreview['attachment_set'] = $this->get_attachment_set($reviewerSubmission);
 			$recommendation = $reviewAssignment->getRecommendation();
             $rqcreview['suggested_decision'] = $recommendation ? $this->rqc_decision("reviewer", $recommendation) : "";
 			//--- reviewer:
@@ -215,18 +255,19 @@ class RqcData extends RqcDevHelper {
 
 
 	/**
-	 * Obtain what is to be considered the text of the view for case 1.
+	 * Obtain what is to be considered the text of the review for case 1.
 	 * Goes through the review form elements,
 	 * works on the REVIEW_FORM_ELEMENT_TYPE_TEXTAREA fields only,
 	 * producing a stretch of output text for each, using
 	 * 1. the element's name as a heading and
 	 * 2. the corresponding ReviewFormResponse's value as body.
 	 */
-	protected function getReviewTextFromForm(ReviewerSubmission $reviewerSubmission, int $reviewFormId): string {
-		$this->_print("##### getReviewTextFromForm\n");
+	protected function getReviewTextFromForm(ReviewerSubmission $reviewerSubmission, int $reviewFormId): string
+	{
+		$reviewFormElementDao = DAORegistry::getDAO('ReviewFormElementDAO');
+		$reviewFormResponseDao = DAORegistry::getDAO('ReviewFormResponseDAO');
 		$reviewId = $reviewerSubmission->getReviewId();
-		// $reviewForm = $this->reviewFormDao->getById($reviewFormId);
-		$reviewFormElements = $this->reviewFormElementDao->getByReviewFormId($reviewFormId);
+		$reviewFormElements = $reviewFormElementDao->getByReviewFormId($reviewFormId);
 		$result = "";
 		while ($reviewFormElement = $reviewFormElements->next()) {
 			$this->_print("### reviewFormElement.elementType=" . $reviewFormElement->getElementType() .
@@ -237,10 +278,10 @@ class RqcData extends RqcDevHelper {
 				$elementTitle = $reviewFormElement->getQuestion('en_US');  // may have HTML tags!
 				$elementTitle = str_replace('<p>', '', $elementTitle);
 				$elementTitle = str_replace('</p>', '', $elementTitle);
-				$responseElement = $this->reviewFormResponseDao->getReviewFormResponse($reviewId, $reviewFormElementId);
+				$responseElement = $reviewFormResponseDao->getReviewFormResponse($reviewId, $reviewFormElementId);
 				$responseText = $this->cleanPlaintextTextarea($responseElement->getValue());
 				if (!preg_match(self::CONFIDENTIAL_FIELD_REGEXP, $elementTitle)) {
-					$result .= "\n### $elementTitle\n\n$responseText\n\n";
+					$result .= "\n### $elementTitle\n\n$responseText\n\n";  // plain-text-format this element
 				}
 			}
 		}
@@ -248,9 +289,10 @@ class RqcData extends RqcDevHelper {
 	}
 
 	/**
-	 * Obtain what is to be considered the text of the view for case 2.
+	 * Obtain what is to be considered the text of the review for case 2.
 	 */
-	protected static function getReviewTextDefault(ReviewAssignment $reviewAssignment): string {
+	protected function getReviewTextDefault(ReviewAssignment $reviewAssignment): string
+	{
 		$submissionCommentDao = DAORegistry::getDAO('SubmissionCommentDAO');
 		/* @var $submissionCommentDao SubmissionCommentDAO */
 		$viewableOnly = true;  // will automatically skip confidential comment
@@ -259,20 +301,23 @@ class RqcData extends RqcDevHelper {
 			$reviewAssignment->getReviewerId(), $reviewAssignment->getId(), $viewableOnly);
 		$result = "";
 		while($submissionComment = $submissionComments->next()) {
-			if ($submissionComment->getCommentType() != COMMENT_TYPE_PEER_REVIEW)
+			if ($submissionComment->getCommentType() != COMMENT_TYPE_PEER_REVIEW) {
 				continue;  // irrelevant record, skip it
+			}
 			$title = $submissionComment->getCommentTitle();  // will be empty
 			$body = $submissionComment->getComments();
-			$result .= $title ? "\n<div>$title</div>\n\n$body\n\n" : "\n$body\n";
+			$result .= ($title ? "\n<div>$title</div>\n\n$body\n\n" : "\n$body\n");
+
 		}
-		return $result;
+		return str_replace("\r", '', $result);  // may contain CR LF, we want only LF
 	}
 
 	/**
 	 * Get first english title if one exists or all titles otherwise.
 	 * @param array $all_titles  mapping from locale name to title string
 	 */
-	protected static function get_title($all_titles) {
+	protected static function get_title(array $all_titles): string
+	{
 		return englishest($all_titles, true);
 	}
 
@@ -282,11 +327,11 @@ class RqcData extends RqcDevHelper {
 	 * if round is 0 (for a non-existing predecessor), return "".
 	 * We could use $lastReviewRound->getId(), but don't.
 	 */
-     protected static function get_uid($journal, $submission, $round, $for_url=false) {
+     protected static function get_uid($journal, $submission, $round, $for_url=false): string
+	 {
 		if ($round == 0) {
 			return "";
-		}
-		else {
+		} else {
 			$journalname = $journal->getPath();
 			$submission_id = $submission->getId();
 			if ($for_url) {
@@ -302,7 +347,8 @@ class RqcData extends RqcDevHelper {
      * Helper: Translate OJS recommendations and decisions into RQC decisions.
      * For editors, we use decisions only and return unknown for recommendations.
      */
-    protected static function rqc_decision($role, $ojs_decision) {
+    protected static function rqc_decision($role, $ojs_decision)
+	{
         $reviewerMap = array(
             // see lib.pkp.classes.submission.reviewAssignment.ReviewAssignment
             // the values are 1,2,3,4,5,6
@@ -340,9 +386,9 @@ class RqcData extends RqcDevHelper {
 	/**
 	 * Helper: Remove possible unwanted properties from text coming from textarea fields.
 	 */
-	protected static function cleanPlaintextTextarea($text): string {
-		$text = str_replace('\r', '', $text);  // may contain CR LF, we want only LF
-		return $text;
+	protected static function cleanPlaintextTextarea($text): string
+	{
+		return str_replace("\r", '', $text);  // may contain CR LF, we want only LF
 	}
 }
 
@@ -351,7 +397,7 @@ class RqcOjsData {
 	/**
 	 * Helper: Discriminate decisions from recommendations.
 	 */
-	public static function is_decision($ojs_decision)
+	public static function is_decision($ojs_decision): bool
 	{
 		switch ($ojs_decision) {
 			case SUBMISSION_EDITOR_DECISION_ACCEPT:
@@ -371,7 +417,8 @@ class RqcOjsData {
  * the entry of the alphabetically first locale otherwise.
  * @param array $all_entries  mapping from locale name to string
  */
-function englishest($all_entries, $else_all=false) {
+function englishest(array $all_entries, $else_all=false)
+{
 	$all_nonenglish_locales = array();
 	foreach ($all_entries as $locale => $entry) {
 		if (substr($locale, 0, 2) === "en") {
@@ -387,8 +434,7 @@ function englishest($all_entries, $else_all=false) {
 	}
 	if ($else_all) {
 		return implode(" / ", $all_nonenglish_entries);
-	}
-	else {
+	} else {
 		return $all_nonenglish_entries[0];
 	}
 }
@@ -397,7 +443,8 @@ function englishest($all_entries, $else_all=false) {
  * Helper: Obtain an attribute value in the most sensible localized version we can identify:
  * RQC_LOCALE if possible, the englishest available one otherwise.
  */
-function get_nonlocalized_attr($obj, $functionname) {
+function get_nonlocalized_attr($obj, $functionname): string
+{
 	$result = $obj->$functionname(RQC_LOCALE);  // get preferred value
 	if (!$result) {  // get random first value from full localized list of attribute values
 		$all_localized_attrs = $obj->$functionname(null);
@@ -413,7 +460,8 @@ function get_nonlocalized_attr($obj, $functionname) {
 /**
  * Helper: Obtain the ORCID ID number from an ORCID URL as stored in OJS.
  */
-function get_orcid_id($orcid_url) {
+function get_orcid_id($orcid_url): string
+{
 	$pattern = '/(https?:\/\/\w+\.\w+\/)?(\d+-\d+-\d+-\d+)/';
 	$success = preg_match($pattern, $orcid_url, $match);
 	return $success ? $match[2] : "";
@@ -424,9 +472,10 @@ function get_orcid_id($orcid_url) {
 /**
  * Helper: Transform timestamp format to RQC convention.
  */
-function rqcify_datetime($ojs_datetime) {
+function rqcify_datetime($ojs_datetime): string
+{
 	if (!$ojs_datetime) {
-		return NULL;
+		return "";
 	}
 	$result = str_replace(" ", "T", $ojs_datetime);
 	return $result . "Z";
