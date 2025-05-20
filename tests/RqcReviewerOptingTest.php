66296dbeb372ee268719fee7bbc3e9030592bb7e
Jakob Ring <ringj02@zedat.fu-berlin.de>
2025-05-20 18:46:57 +0200

    adapted the first tests to 3.5. First try (not finished)

--- tests/RqcReviewerOptingTest.php
+++ tests/RqcReviewerOptingTest.php
@@ -1,105 +1,193 @@
 <?php
 
+namespace APP\plugins\generic\rqc\tests;
+
+use APP\plugins\generic\rqc\classes\DelayedRqcCall\DelayedRqcCallDAO;
+use APP\plugins\generic\rqc\RqcPlugin;
+use APP\plugins\generic\rqc\tests\RqcTestHelper;
+use APP\plugins\generic\rqc\classes\RqcReviewerOpting\RqcReviewerOptingDAO;
+use APP\plugins\generic\rqc\classes\ReviewerOpting;
+
+use PKP\db\DAORegistry;
+use PKP\tests\DatabaseTestCase;
+use PKP\tests\PKPTestHelper;
+use PKP\tests\plugins\PluginTestCase;
+
+//require_mock_env('env2'); // Required for mock app locale.
+
 /**
  * Test reviewer opt-in/opt-out functionality.
  *
  * @ingroup plugins_generic_rqc
  */
-
-require_once('lib/pkp/tests/phpunit-bootstrap.php');
-//require_mock_env('env2'); // Required for mock app locale.
-
-import('lib.pkp.tests.DatabaseTestCase');
-import('lib.pkp.classes.db.DAORegistry');
-//import('lib.pkp.classes.core.PKPRouter');
-
-import('plugins.generic.rqc.classes.ReviewerOpting');
-import('plugins.generic.rqc.tests.helpers');
-
-class RqcReviewerReviewerOptingTest extends DatabaseTestCase
+class RqcReviewerOptingTest extends PluginTestCase
 {
 
-	protected function getAffectedTables()
-	{
-		return array('users', 'user_settings');
+	protected function getAffectedTables(): int|array
+    {
+        // complete DB reset is available by returning PKP_TEST_ENTIRE_DB
+        //return PKPTestHelper::PKP_TEST_ENTIRE_DB; // doesn't work for me
+        return array( // the order matters!!
+            'rqc_reviewer_opting_settings', 'rqc_reviewer_opting',
+            'review_assignments',
+            'review_rounds',
+            'author_settings', 'authors',
+            'user_settings', 'users',
+            'publication_settings', 'publications',
+            'submission_settings', 'submissions',
+            'journals', 'journal_settings'
+        );
 	}
 
 	protected function setUp(): void
 	{
 		parent::setUp();
-		$this->reviewer11 = make_user("reviewer11");
-		$this->assertTrue($this->reviewer11->getId() > 0);  // ensure that make_user() writes to DB
+        $helper = new RqcTestHelper();
+		$this->reviewer11 = $helper->make_and_store_user("reviewer11");
+        $this->request11 = $this->mockRequest("testJournal/test", $this->reviewer11->getId());
+        $this->context11 = $helper->make_and_store_context("testJournal", $this->request11);
+        $this->submission11 = $helper->make_and_store_reviewable_submission($this->context11, [], [$this->reviewer11]);
+        $this->submission12 = $helper->make_and_store_reviewable_submission($this->context11, [], [$this->reviewer11]);
+		$this->assertTrue($this->reviewer11->getId() > 0);  // ensure that make_and_store_user() writes to DB
+        $this->assertTrue($this->context11->getId() > 0);  // ensure that make_and_store_context() writes to DB
+        $this->assertTrue($this->submission11->getId() > 0);  // ensure that make_and_store_reviewable_submission() writes to DB
+        DAORegistry::registerDAO('DelayedRqcCallDAO', new DelayedRqcCallDAO());
+        DAORegistry::registerDAO('RqcReviewerOptingDAO', new RqcReviewerOptingDAO());
 	}
 
 	public function testReviewerOpting()
 	{
-		$context = 1;  // fictive, does not really exist
+		$contextId = $this->context11->getId();  // fictive, does not really exist
 		$user = $this->reviewer11;
+        $submission1 = $this->submission11;
+        $submission2 = $this->submission12;
+
 		$opting = new ReviewerOpting();
 		$this->expectOutputString('');  // report any debugging output we print during development
-		//----- check undefined initial case:
-		$status = $opting->getStatus($context, $user, !RQC_PRELIM_OPTING);
-		$this->assertEquals(RQC_OPTING_STATUS_UNDEFINED, $status);
-		$status = $opting->getStatus($context, $user, RQC_PRELIM_OPTING);
-		$this->assertEquals(RQC_OPTING_STATUS_UNDEFINED, $status);
-		//----- check storing IN preliminarily:
-		$opting->setStatus($context, $user, RQC_OPTING_STATUS_IN, RQC_PRELIM_OPTING);
-		$status = $opting->getStatus($context, $user, RQC_PRELIM_OPTING);
-		$this->assertEquals(RQC_OPTING_STATUS_IN, $status);
-		$status = $opting->getStatus($context, $user);
-		$this->assertEquals(RQC_OPTING_STATUS_UNDEFINED, $status);
-		//----- check storing OUT preliminarily:
-		$opting->setStatus($context, $user, RQC_OPTING_STATUS_OUT, RQC_PRELIM_OPTING, "2024");
-		$status = $opting->getStatus($context, $user, RQC_PRELIM_OPTING, "2024");
-		$this->assertEquals(RQC_OPTING_STATUS_OUT, $status);
-		$status = $opting->getStatus($context, $user, !RQC_PRELIM_OPTING, "2024");
-		$this->assertEquals(RQC_OPTING_STATUS_UNDEFINED, $status);
+
+        //----- check undefined initial case:
+		$this->assertEquals(RQC_OPTING_STATUS_UNDEFINED, $opting->getUserOptingStatus($contextId, $user, !RQC_PRELIM_OPTING));
+		$this->assertEquals(RQC_OPTING_STATUS_UNDEFINED, $opting->getUserOptingStatus($contextId, $user, RQC_PRELIM_OPTING));
+        $this->assertTrue($opting->userOptingRequired($contextId, $user));
+        $this->assertFalse($opting->isOptedOut($submission1, $user));
+        $this->assertFalse($opting->isOptedOut($submission2, $user));
+
+        //----- check storing IN preliminarily:
+		$opting->setOptingStatus($contextId, $submission1, $user, RQC_OPTING_STATUS_IN, RQC_PRELIM_OPTING);
+
+        $this->assertEquals(RQC_OPTING_STATUS_UNDEFINED, $opting->getUserOptingStatus($contextId, $user, !RQC_PRELIM_OPTING));
+        $this->assertEquals(RQC_OPTING_STATUS_IN, $opting->getUserOptingStatus($contextId, $user, RQC_PRELIM_OPTING));
+        $this->assertTrue($opting->userOptingRequired($contextId, $user));
+        $this->assertFalse($opting->isOptedOut($submission1, $user));
+        $this->assertFalse($opting->isOptedOut($submission2, $user));
+
+        //----- check storing OUT preliminarily:
+        $opting->setOptingStatus($contextId, $submission1, $user, RQC_OPTING_STATUS_OUT, RQC_PRELIM_OPTING);
+
+        $this->assertEquals(RQC_OPTING_STATUS_UNDEFINED, $opting->getUserOptingStatus($contextId, $user, !RQC_PRELIM_OPTING));
+        $this->assertEquals(RQC_OPTING_STATUS_OUT, $opting->getUserOptingStatus($contextId, $user, RQC_PRELIM_OPTING));
+        $this->assertTrue($opting->userOptingRequired($contextId, $user));
+        $this->assertFalse($opting->isOptedOut($submission1, $user));
+        $this->assertFalse($opting->isOptedOut($submission2, $user));
+
 		//----- check storing IN:
-		$opting->setStatus($context, $user, RQC_OPTING_STATUS_IN, date('Y'));
-		$status = $opting->getStatus($context, $user, RQC_PRELIM_OPTING, date('Y'));
-		$this->assertEquals(RQC_OPTING_STATUS_IN, $status);
-		$status = $opting->getStatus($context, $user);
-		$this->assertEquals(RQC_OPTING_STATUS_IN, $status);
+        $opting->setOptingStatus($contextId, $submission1, $user, RQC_OPTING_STATUS_IN, !RQC_PRELIM_OPTING, date('Y'));
+
+        $this->assertEquals(RQC_OPTING_STATUS_IN, $opting->getUserOptingStatus($contextId, $user, !RQC_PRELIM_OPTING));
+        $this->assertEquals(RQC_OPTING_STATUS_IN, $opting->getUserOptingStatus($contextId, $user, RQC_PRELIM_OPTING));
+        $this->assertFalse($opting->userOptingRequired($contextId, $user));
+        $this->assertFalse($opting->isOptedOut($submission1, $user));
+        $this->assertFalse($opting->isOptedOut($submission2, $user));
+
 		//----- check storing OUT:
-		$opting->setStatus($context, $user, RQC_OPTING_STATUS_OUT);
-		$status = $opting->getStatus($context, $user, RQC_PRELIM_OPTING);
-		$this->assertEquals(RQC_OPTING_STATUS_OUT, $status);
-		$status = $opting->getStatus($context, $user);
-		$this->assertEquals(RQC_OPTING_STATUS_OUT, $status);
-	}
+        $opting->setOptingStatus($contextId, $submission1, $user, RQC_OPTING_STATUS_OUT, !RQC_PRELIM_OPTING);
+
+        $this->assertEquals(RQC_OPTING_STATUS_OUT, $opting->getUserOptingStatus($contextId, $user, !RQC_PRELIM_OPTING));
+        $this->assertEquals(RQC_OPTING_STATUS_OUT, $opting->getUserOptingStatus($contextId, $user, RQC_PRELIM_OPTING));
+        $this->assertFalse($opting->userOptingRequired($contextId, $user));
+        $this->assertTrue($opting->isOptedOut($submission1, $user));
+        $this->assertFalse($opting->isOptedOut($submission2, $user));
+
+        //----- check opting with other years and submissions
+        $this->assertTrue($opting->userOptingRequired($contextId, $user, "2024"));
+
+        $opting->setOptingStatus($contextId, $submission2, $user, RQC_OPTING_STATUS_IN, RQC_PRELIM_OPTING, "2024");
+
+        $this->assertEquals(RQC_OPTING_STATUS_OUT, $opting->getUserOptingStatus($contextId, $user, !RQC_PRELIM_OPTING));
+        $this->assertEquals(RQC_OPTING_STATUS_OUT, $opting->getUserOptingStatus($contextId, $user, RQC_PRELIM_OPTING));
+        $this->assertEquals(RQC_OPTING_STATUS_UNDEFINED, $opting->getUserOptingStatus($contextId, $user, !RQC_PRELIM_OPTING, "2024"));
+        $this->assertEquals(RQC_OPTING_STATUS_IN, $opting->getUserOptingStatus($contextId, $user, RQC_PRELIM_OPTING, "2024"));
+        $this->assertFalse($opting->userOptingRequired($contextId, $user));
+        $this->assertTrue($opting->userOptingRequired($contextId, $user, "2024"));
+        $this->assertTrue($opting->isOptedOut($submission1, $user));
+        $this->assertFalse($opting->isOptedOut($submission2, $user));
+
+        $opting->setOptingStatus($contextId, $submission2, $user, RQC_OPTING_STATUS_IN, !RQC_PRELIM_OPTING, "2024"); // and now without preliminary opting
+
+        $this->assertEquals(RQC_OPTING_STATUS_OUT, $opting->getUserOptingStatus($contextId, $user, !RQC_PRELIM_OPTING));
+        $this->assertEquals(RQC_OPTING_STATUS_OUT, $opting->getUserOptingStatus($contextId, $user, RQC_PRELIM_OPTING));
+        $this->assertEquals(RQC_OPTING_STATUS_IN, $opting->getUserOptingStatus($contextId, $user, !RQC_PRELIM_OPTING, "2024"));
+        $this->assertEquals(RQC_OPTING_STATUS_IN, $opting->getUserOptingStatus($contextId, $user, RQC_PRELIM_OPTING, "2024"));
+        $this->assertFalse($opting->userOptingRequired($contextId, $user));
+        $this->assertFalse($opting->userOptingRequired($contextId, $user, "2024"));
+        $this->assertTrue($opting->isOptedOut($submission1, $user));
+        $this->assertFalse($opting->isOptedOut($submission2, $user));
+
+        //----- test the deletion of the 2024er entry and add a 2025 entry both of submission2
+        $rqcReviewerOptingDAO = new rqcReviewerOptingDAO();
+        $rqcReviewerOptingDAO->delete($rqcReviewerOptingDAO->getReviewerOpting($contextId, $submission2->getId(), $user->getId(), "2024"));
+        $opting->setOptingStatus($contextId, $submission2, $user, RQC_OPTING_STATUS_OUT, !RQC_PRELIM_OPTING);
+
+        $this->assertEquals(RQC_OPTING_STATUS_OUT, $opting->getUserOptingStatus($contextId, $user, !RQC_PRELIM_OPTING));
+        $this->assertEquals(RQC_OPTING_STATUS_OUT, $opting->getUserOptingStatus($contextId, $user, RQC_PRELIM_OPTING));
+        $this->assertEquals(RQC_OPTING_STATUS_UNDEFINED, $opting->getUserOptingStatus($contextId, $user, !RQC_PRELIM_OPTING, "2024"));
+        $this->assertEquals(RQC_OPTING_STATUS_UNDEFINED, $opting->getUserOptingStatus($contextId, $user, RQC_PRELIM_OPTING, "2024"));
+        $this->assertFalse($opting->userOptingRequired($contextId, $user));
+        $this->assertTrue($opting->userOptingRequired($contextId, $user, "2024"));
+        $this->assertTrue($opting->isOptedOut($submission1, $user));
+        $this->assertTrue($opting->isOptedOut($submission2, $user));
+    }
 }
 
 
 class RqcReviewerOptingFormTest extends DatabaseTestCase
 {
 
-	protected function getAffectedTables()
-	{
-		// complete DB reset is available by returning PKP_TEST_ENTIRE_DB
+	protected function getAffectedTables(): int|array
+    {
+        //return PKPTestHelper::PKP_TEST_ENTIRE_DB;
+        // complete DB reset is available by returning PKP_TEST_ENTIRE_DB
 		return array(
-			'authors',
-			'publications', 'publication_settings',
-			'review_assignments',
-			'submissions', 'submission_settings',
-			'users', 'user_settings'
+            'rqc_reviewer_opting_settings', 'rqc_reviewer_opting',
+            'review_assignments',
+            'review_rounds',
+            'author_settings', 'authors',
+            'user_settings', 'users',
+            'publication_settings', 'publications',
+            'submission_settings', 'submissions',
+            'journals', 'journal_settings'
 		);
 	}
 
 	protected function setUp(): void
 	{
 		parent::setUp();
-		$this->context = 1;  // fictive, does not really exist
-		$this->author11 = make_user("author11");
-		$this->reviewer11 = make_user("reviewer11");
-		$this->reviewer12 = make_user("reviewer12");
-		$this->submission = make_reviewable_submission(
-			$this->context, [$this->author11],
-			[$this->reviewer11, $this->reviewer12]);
+        $helper = new RqcTestHelper();
+        $request = $this->mockRequest();
+        $this->context = $helper->make_and_store_context("testJournal", $request);
+		$this->author21 = $helper->make_and_store_user("author21");
+		$this->reviewer21 = $helper->make_and_store_user("reviewer21");
+		$this->reviewer22 = $helper->make_and_store_user("reviewer22");
+		$this->submission = $helper->make_and_store_reviewable_submission(
+			$this->context, [$this->author21],
+			[$this->reviewer21, $this->reviewer22]);
 	}
 
 	public function testOptingFormGET()
 	{
 		// get Step3Form and look for opting question
+        $reviewerOpting = new ReviewerOpting();
+        $reviewerOpting->callbackInitOptingData();
 
 		// TODO 3
 	}
---
+++ tests/RqcTestHelper.php
@@ -0,0 +1,155 @@
+<?php
+
+namespace APP\plugins\generic\rqc\tests;
+
+use APP\core\Application;
+use APP\core\Request;
+use APP\facades\Repo;
+use APP\publication\Publication;
+use APP\submission\Submission;
+use PKP\context\Context;
+use PKP\core\Core;
+use PKP\services\PKPContextService;
+use PKP\submission\PKPSubmission;
+use PKP\submission\reviewAssignment\ReviewAssignment;
+use PKP\user\User;
+
+/**
+ * Routines that help when writing tests, in particular for creating data
+ *
+ * @ingroup plugins_generic_rqc
+ */
+class RqcTestHelper
+{
+//function storeAll(Submission $submission, Publication $publication, Context $context): int
+//{
+//    $submissionId = Repo::submission()->add($submission, $publication, $context);
+//    $submission = Repo::submission()->get($submissionId);
+//
+//    // Assign submitter to submission
+//    Repo::stageAssignment()
+//        ->build(
+//            $submission->getId(),
+//            $submitAsUserGroup->id,
+//            $request->getUser()->getId(),
+//            0,
+//            1
+//        );
+//
+//    // Create an author record from the submitter's user account
+//    if ($submitAsUserGroup->roleId === Role::ROLE_ID_AUTHOR) {
+//        $author = Repo::author()->newAuthorFromUser($request->getUser(), $submission, $context);
+//        $author->setData('publicationId', $publication->getId());
+//        $author->setUserGroupId($submitAsUserGroup->id);
+//        $authorId = Repo::author()->add($author);
+//        Repo::publication()->edit($publication, ['primaryContactId' => $authorId]);
+//    }
+//
+//    $userGroups = UserGroup::withContextIds($submission->getData('contextId'))->cursor();
+//
+//    /** @var GenreDAO $genreDao */
+//    $genreDao = DAORegistry::getDAO('GenreDAO');
+//    $genres = $genreDao->getByContextId($submission->getData('contextId'))->toArray();
+//
+//    if (!$userGroups instanceof LazyCollection) {
+//        $userGroups = $userGroups->lazy();
+//    }
+//
+//    $userRoles = $this->getAuthorizedContextObject(Application::ASSOC_TYPE_USER_ROLES);
+//}
+
+    public function make_and_store_reviewable_submission(Context $context, array $authors, array $reviewers): Submission
+    {
+        // build
+        $newSubmission = $this->make_submission($context);
+        $newPublication = $this->make_publication($newSubmission);
+
+        $newSubmissionId = Repo::submission()->add($newSubmission, $newPublication, $context);
+        $newSubmission = Repo::submission()->get($newSubmissionId);
+
+        // store the authors
+        foreach ($authors as $user) {
+            $author = Repo::author()->newDataObject();
+            $author->_data = [
+                'email' => $user->getEmail(),
+                'publicationId' => $newPublication->getId(),
+            ];
+            Repo::author()->add($author);
+        }
+
+        // make and store the reviewers
+        foreach ($reviewers as $user) {
+            $reviewAssignment = $this->make_and_store_reviewAssignment($newSubmission, $user);
+        }
+        return $newSubmission;
+    }
+
+    public function make_publication(Submission $submission): Publication
+    {
+        $publication = Repo::publication()->newDataObject();
+        $publication->_data = [
+            'submissionId' => $submission->getId(),
+            'title' => 'made ' . date("%Y-%m-%d %H:%M:%S"),
+            'seq' => 0,
+            'access_status' => 0,
+            'lastModified' => Core::getCurrentDate()
+        ];
+        return $publication;
+    }
+
+    public function make_submission(Context $context): Submission
+    {
+        $newSubmission = Repo::submission()->newDataObject();
+        $newSubmission->_data = [
+            'contextId' => $context,
+            'stageId' => WORKFLOW_STAGE_ID_EXTERNAL_REVIEW,
+            'status' => PKPSubmission::STATUS_QUEUED,  // as in manually created case
+            'submissionProgress' => 0,  // as in manually created case
+            'dateLastActivity' => Core::getCurrentDate(),
+            'dateSubmitted' => Core::getCurrentDate(),
+            'lastModified' => Core::getCurrentDate()
+        ];
+        return $newSubmission;
+    }
+
+    public function make_and_store_reviewAssignment(Submission $submission, User $user): ReviewAssignment
+    {
+        $reviewAssignment = Repo::reviewAssignment()->newDataObject();
+        $reviewAssignment->_data = [
+            'submissionId' => $submission->getId(),  // Huh? Not a publication?
+            'reviewerId' => $user->getId(),
+            'reviewRoundId' => 1,
+            'stageId' => WORKFLOW_STAGE_ID_EXTERNAL_REVIEW,
+            // reviewFormId appears to be optional
+        ];
+        $reviewAssignmentId = Repo::reviewAssignment()->add($reviewAssignment);
+        return Repo::reviewAssignment()->get($reviewAssignmentId);  // fetch clean from db
+    }
+
+    public function make_and_store_user(string $username): User
+    {
+        $user = Repo::user()->newDataObject();
+        $user->setUsername($username);
+        $user->setPassword('1234');
+        $user->setGivenname($username[0], RQC_LOCALE);
+        $user->setFamilyname($username, RQC_LOCALE);
+        $user->setEmail($username . '@some.where');
+        $user->setDateRegistered(Core::getCurrentDate());
+        $userId = Repo::user()->add($user);
+        return Repo::user()->get($userId); // fetch clean from db
+    }
+
+    public function make_and_store_context(string $path, Request $request): Context
+    {
+        $contextService = app()->get('context');
+        /** @var PKPContextService $contextService */
+        $context = Application::getContextDAO()->newDataObject();
+        $context->setAllData([
+            'seq' => 1,
+            'primary_locale' => 'en',
+            'enabled' => 1
+        ]);
+        $context->setPath($path);
+        return $contextService->add($context, $request);
+    }
+}
